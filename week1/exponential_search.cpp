467d25af7238b79cb590f274152dc7ef470160ba
suhanit04 <thapliyalsuhani@gmail.com>
2021-07-25 16:13:14 +0530

    exponential search updated

--- week1/exponential_search.cpp
+++ week1/exponential_search.cpp
@@ -14,8 +14,11 @@ int main()
         cin>>k;
         int flag=0,c=0;
         if(a[0]==k){
-          cout<<"Present "<<1;
+          c++;
+          cout<<"Present "<<c;
         }
+        else
+         c++;
         int i=1;
         while(i<n&& a[i]<=k)
         {
@@ -25,19 +28,21 @@ int main()
         int mid, beg=i/2, e=min(i,n-1);
         while(beg<=e)
       {
-          c++;
          mid=(beg+e)/2;
          if(a[mid]==k)
          {
+             c++;
              cout<<"Present "<<c<<endl;
              flag=1;
              break;
 
          }
          else if(k<a[mid])
-             e=mid-1;
+             {c++;
+             e=mid-1;}
         else
-             beg=mid+1;
+             {c++;
+             beg=mid+1;}
       }
      if(flag==0)
         {cout<<"Not Present ";
