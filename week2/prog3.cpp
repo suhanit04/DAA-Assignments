7ba5c936c236cd0ee7619d249fe870a221366da0
suhanit04 <thapliyalsuhani@gmail.com>
2021-08-06 14:22:16 +0530

    Week2

---
+++ week2/prog3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t>0)
+    {
+        int n,key,c=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        cin>>key;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<=n;j++)
+            {
+                if(abs(a[i]-a[j])==key)
+                 c++;
+            }
+        }
+        cout<<c<<endl;
+    t--;
+    }
+    return 0;
+}
